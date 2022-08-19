fe27e531fed037125f0223d2558bd65c36d45731
Thiago Viana <trodrigo734@gmail.com>
2022-08-19 16:02:15 -0300

    edit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 function Login() {
+  const handleChange = ({ target }, func = '') => {
+    func(target.value);
+  };
+
   return (
     <form>
       <input
         type="email"
         data-testid="email-input"
+        onChange={ (e) => handleChange(e) }
       />
 
       <input
