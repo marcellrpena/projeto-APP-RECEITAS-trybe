392a07a53a77b7a8b483c2cf5286b3bfacf78c9f
Lauro Pereira <lauropera01@gmail.com>
2022-08-22 15:12:50 -0300

    SearchBar - Requisito 10 Concluído

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,37 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <h1>Search Bar</h1>
+    <div>
+      <form>
+        <label htmlFor="igredient-radio">
+          <input
+            type="radio"
+            id="igredient-radio"
+            data-testid="ingredient-search-radio"
+          />
+          Igredientes
+        </label>
+        <label htmlFor="search-radio">
+          <input
+            type="radio"
+            id="search-radio"
+            data-testid="name-search-radio"
+          />
+          Nome
+        </label>
+        <label htmlFor="first-letter-radio">
+          <input
+            type="radio"
+            id="first-letter-radio"
+            data-testid="first-letter-search-radio"
+          />
+          Primeira Letra
+        </label>
+        <button type="submit" data-testid="exec-search-btn">
+          Pesquisar
+        </button>
+      </form>
+    </div>
   );
 }
 
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SearchBar from '../components/SearchBar';
 
 function Foods() {
   return (
     <div>
       <h1>Foods</h1>
+      <SearchBar />
+
     </div>
   );
 }
