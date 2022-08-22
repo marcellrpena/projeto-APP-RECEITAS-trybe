1e78be4290b09c2d704d4ef7cc0bc8aba366b22c
Lauro Pereira <lauropera01@gmail.com>
2022-08-22 15:51:30 -0300

    Corrigindo icone do profile no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,36 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
 
 function Header({ name, props: { history } }) {
   const [searchBar, setSearchBar] = useState(false);
   const names = ['Profile', 'Done Recipes', 'Favorite Recipes'];
   return (
     <header>
-      <input
-        type="button"
-        data-testid="profile-top-btn"
-        onClick={ () => history.push('/profile') }
-        src="src/images/profileIcon.svg"
-        alt="profilePhoto"
-      />
-      <h1 data-testid="page-title">{name}</h1>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-top-btn"
+          onClick={ () => history.push('/profile') }
+          src={ profileIcon }
+          alt="User icon"
+        >
+          <img src={ profileIcon } alt="User icon" />
+        </button>
+        <h1 data-testid="page-title">{name}</h1>
+      </div>
       {!names.includes(name) && (
         <div>
-          <div>
-            <button
-              type="button"
-              src={ searchIcon }
-              alt="Search Icon"
-              data-testid="search-top-btn"
-              onClick={ () => setSearchBar(!searchBar) }
-            >
-              <img src={ searchIcon } alt="Search Icon" />
-            </button>
-          </div>
+          <button
+            type="button"
+            src={ searchIcon }
+            alt="Search Icon"
+            data-testid="search-top-btn"
+            onClick={ () => setSearchBar(!searchBar) }
+          >
+            <img src={ searchIcon } alt="Search Icon" />
+          </button>
           {searchBar && <input type="text" data-testid="search-input" />}
         </div>
       )}
