94f3eede6ea533ad0bd58364f51da781b96b4a29
Lauro Pereira <lauropera01@gmail.com>
2022-08-23 12:25:05 -0300

    Melhorando testes

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import renderWithRouter from './helpers/renderWithRouter';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LoginProvider from '../contexts/LoginProvider';
 import App from '../App';
 
 describe('Testes do componente Header', () => {
-  it('Testa se é redirecionado para a rota "/foods" depois de fazer login', () => {
-    const { history } = renderWithRouter(
-      <LoginProvider>
-        <App />
-      </LoginProvider>
-    );
-    const emailInput = screen.getByTestId('email-input');
-    const passInput = screen.getByTestId('password-input');
-    const loginBtn = screen.getByTestId('login-submit-btn');
-    userEvent.type(emailInput, 'test@test.com');
-    userEvent.type(passInput, '1234567');
-    userEvent.click(loginBtn);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/foods');
-  });
-
   describe('Testa o clique no icone Profile do header', () => {
     it('Testa se ao clicar no icone de profile é redirecionado para página de profile', () => {
       const { history } = renderWithRouter(
@@ -42,7 +26,7 @@ describe('Testes do componente Header', () => {
       expect(pathname).toBe('/profile');
     });
     it('Testa se ao clicar no icone de Seach uma barra de busca aparece', () => {
-      const { history } = renderWithRouter(
+      renderWithRouter(
         <LoginProvider>
           <App />
         </LoginProvider>
--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -6,11 +6,12 @@ import LoginProvider from '../contexts/LoginProvider';
 import RecipesProvider from '../contexts/RecipesProvider';
 import App from '../App';
 import { meals } from '../../cypress/mocks/meals';
+import { drinks } from '../../cypress/mocks/drinks';
 
 describe('Testes da barra de pesquisa', () => {
   afterEach(() => jest.resetAllMocks());
 
-  it('Testa se o botão "Search" está desativado quando os inputs estão vazios', async () => {
+  it('Testa se o botão "Search" está desativado quando os inputs estão vazios', () => {
     expect.assertions(1);
 
     renderWithRouter(
@@ -35,7 +36,7 @@ describe('Testes da barra de pesquisa', () => {
     expect(searchBtn).toBeDisabled();
   });
 
-  it('Testa se é chamado a API de comidas quando está na rota "/foods"', async () => {
+  it('Testa se é chamado a API de comidas quando está na rota "/foods" e faz uma pesquisa', () => {
     expect.assertions(4);
 
     jest.spyOn(global, 'fetch');
@@ -75,5 +76,109 @@ describe('Testes da barra de pesquisa', () => {
 
     const URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=banana';
     expect(fetch).toHaveBeenCalledWith(URL);
+  });
+
+  it('Testa se é chamado a API de bebidas quando está na rota "/drinks" e faz uma pesquisa', () => {
+    expect.assertions(1);
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinks),
+    });
+
+    const { history } = renderWithRouter(
+      <LoginProvider>
+        <RecipesProvider>
+          <App />
+        </RecipesProvider>
+      </LoginProvider>
+    );
+
+    const emailInput = screen.getByTestId('email-input');
+    const passInput = screen.getByTestId('password-input');
+    const loginBtn = screen.getByTestId('login-submit-btn');
+    userEvent.type(emailInput, 'test@test.com');
+    userEvent.type(passInput, '1234567');
+    userEvent.click(loginBtn);
+    history.push('/drinks');
+
+    const showSearchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(showSearchBtn);
+
+    const searchInput = screen.getByTestId('search-input');
+    const nameRadio = screen.getByTestId('name-search-radio');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+    userEvent.type(searchInput, 'banana');
+    userEvent.click(nameRadio);
+    userEvent.click(searchBtn);
+
+    const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=banana';
+    expect(fetch).toHaveBeenCalledWith(URL);
+  });
+
+  it('Testa se traz comidas pelo filtro "First-Letter"', () => {
+    expect.assertions(1);
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinks),
+    });
+
+    renderWithRouter(
+      <LoginProvider>
+        <RecipesProvider>
+          <App />
+        </RecipesProvider>
+      </LoginProvider>
+    );
+
+    const emailInput = screen.getByTestId('email-input');
+    const passInput = screen.getByTestId('password-input');
+    const loginBtn = screen.getByTestId('login-submit-btn');
+    userEvent.type(emailInput, 'test@test.com');
+    userEvent.type(passInput, '1234567');
+    userEvent.click(loginBtn);
+
+    const showSearchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(showSearchBtn);
+
+    const searchInput = screen.getByTestId('search-input');
+    const fistLetterRadio = screen.getByTestId('first-letter-search-radio');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+    userEvent.click(fistLetterRadio);
+    userEvent.type(searchInput, 'b');
+    userEvent.click(searchBtn);
+
+    const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+    expect(fetch).toHaveBeenCalledWith(URL);
+  });
+
+  it('Deve aparecer um alerta quando colocar mais de uma letra no input e selecionar "First-Letter"', () => {
+    expect.assertions(2);
+    global.alert = jest.fn();
+
+    renderWithRouter(
+      <LoginProvider>
+        <RecipesProvider>
+          <App />
+        </RecipesProvider>
+      </LoginProvider>
+    );
+
+    const emailInput = screen.getByTestId('email-input');
+    const passInput = screen.getByTestId('password-input');
+    const loginBtn = screen.getByTestId('login-submit-btn');
+    userEvent.type(emailInput, 'test@test.com');
+    userEvent.type(passInput, '1234567');
+    userEvent.click(loginBtn);
+
+    const showSearchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(showSearchBtn);
+
+    const searchInput = screen.getByTestId('search-input');
+    const fistLetterRadio = screen.getByTestId('first-letter-search-radio');
+    userEvent.click(fistLetterRadio);
+    userEvent.type(searchInput, 'co');
+
+    expect(global.alert).toHaveBeenCalled();
+    expect(searchInput).toHaveValue('c');
   });
 });
