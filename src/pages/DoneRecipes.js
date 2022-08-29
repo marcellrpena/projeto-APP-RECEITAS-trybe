16c73872bae81dd4fa4fef2b5de259486e327938
Thiago Viana <trodrigo734@gmail.com>
2022-08-29 18:15:37 -0300

    Inicia a tela de Receitas favoritas

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,152 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import clipboardCopy from 'clipboard-copy';
+import React from 'react';
 
 import Header from '../components/Header';
-import shareIcon from '../images/shareIcon.svg';
+import RecipesList from '../components/RecipesList';
 
 function DoneRecipes() {
-  const [copyed, setCopyed] = useState(false);
-  const [recipesList, setRecipesList] = useState([]);
-
-  const history = useHistory();
-
-  const localDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-
-  const shareRecipe = (type, id) => {
-    const link = `http://localhost:3000/${type === 'food' ? 'foods' : 'drinks'}/${id}`;
-    clipboardCopy(link);
-    setCopyed(true);
-  };
-
-  const filterRecipes = (filter) => {
-    switch (filter) {
-    case 'all':
-      setRecipesList(localDoneRecipes);
-      break;
-    case 'food':
-      setRecipesList(localDoneRecipes.filter((recipe) => recipe.type === 'food'));
-      break;
-    case 'drink':
-      setRecipesList(localDoneRecipes.filter((recipe) => recipe.type === 'drink'));
-      break;
-    default:
-      setRecipesList(localDoneRecipes);
-      break;
-    }
-  };
-
-  const ToRecipePage = (type, id) => history.push(
-    `/${type === 'food' ? 'foods' : 'drinks'}/${id}`,
-  );
-
-  useEffect(() => {
-    filterRecipes(undefined);
-  }, []);
-
   return (
     <>
       <Header name="Done Recipes" />
-      <nav>
-        <button
-          type="button"
-          data-testid="filter-by-all-btn"
-          onClick={ () => filterRecipes('all') }
-        >
-          All
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-food-btn"
-          onClick={ () => filterRecipes('food') }
-        >
-          Food
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-drink-btn"
-          onClick={ () => filterRecipes('drink') }
-        >
-          Drink
-        </button>
-        <main>
-          <ul>
-            {
-              recipesList.map(({
-                id,
-                image,
-                name,
-                category,
-                nationality,
-                alcoholicOrNot,
-                type,
-                doneDate,
-                tags },
-              index) => (
-                <li key={ id }>
-                  <button
-                    type="button"
-                    onClick={ () => ToRecipePage(type, id) }
-                  >
-                    <img
-                      src={ image }
-                      alt={ name }
-                      data-testid={ `${index}-horizontal-image` }
-                      width="100"
-                    />
-                  </button>
-
-                  <button
-                    type="button"
-                    onClick={ () => ToRecipePage(type, id) }
-                  >
-                    <h1 data-testid={ `${index}-horizontal-name` }>
-                      { name }
-                    </h1>
-                  </button>
-
-                  <p data-testid={ `${index}-horizontal-top-text` }>
-                    {`${type === 'food' ? nationality : alcoholicOrNot} - ${category}`}
-                  </p>
-
-                  <p data-testid={ `${index}-horizontal-done-date` }>
-                    { doneDate }
-                  </p>
-
-                  <button
-                    type="button"
-                    data-testid={ `${index}-horizontal-share-btn` }
-                    src={ shareIcon }
-                    onClick={ () => shareRecipe(type, id) }
-                  >
-                    <img src={ shareIcon } alt="search icon" />
-                  </button>
-
-                  { tags.map((tag) => (
-                    <p
-                      key={ tag }
-                      data-testid={ `${index}-${tag}-horizontal-tag` }
-                    >
-                      { tag }
-                    </p>
-                  ))}
-                </li>
-              ))
-            }
-          </ul>
-          {copyed && (
-            <div style={ { display: 'flex' } }>
-              <p>Link copied!</p>
-              <button
-                type="button"
-                onClick={ () => setCopyed(false) }
-              >
-                x
-              </button>
-            </div>
-          )}
-        </main>
-      </nav>
+      <RecipesList
+        props={ {
+          key: 'doneRecipes',
+          useTags: true,
+        } }
+      />
     </>
   );
 }
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,9 +1,19 @@
 import React from 'react';
+
 import Header from '../components/Header';
+import RecipesList from '../components/RecipesList';
 
 function FavoriteRecipes() {
   return (
-    <Header name="Favorite Recipes" />
+    <>
+      <Header name="Favorite Recipes" />
+      <RecipesList
+        props={ {
+          key: 'favoriteRecipes',
+          useTags: false,
+        } }
+      />
+    </>
   );
 }
 
