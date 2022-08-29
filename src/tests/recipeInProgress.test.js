e370af99054993a8b91ecdd8b0be15a0613dd025
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-08-29 12:33:24 -0300

    finalizado testes do requisito 43

--- src/tests/recipeInProgress.test.js
+++ src/tests/recipeInProgress.test.js
@@ -5,16 +5,13 @@ import renderWithRouterAndContext from './helpers/renderWithRouterAndContext';
 import meals from '../../cypress/mocks/meals';
 import drinks from '../../cypress/mocks/drinks';
 import App from '../App';
+import clipboardCopy from 'clipboard-copy';
+//import clipboardCopy from 'clipboard-copy';
 
+jest.mock('clipboard-copy', () => jest.fn());
+const mockClipboard = require('clipboard-copy');
+// https://trybecourse.slack.com/archives/C01T2C18DSM/p1630099534116900?thread_ts=1630092847.100100&cid=C01T2C18DSM
 describe('Testes da pagina de receitas em progresso', () => {
- /*  beforeEach(() => {
-    jest.spyOn(global, 'fetch');
-    global.fetch.mockResolvedValue({
-      json: jest.fn().mockResolvedValue(meals),
-    });
-  });
-
-  afterEach(() => jest.resetAllMocks()); */
 
   it('Testa se, após navegar para receitas em progresso com um id de receita a página é renderizada para aquela receita', async () => {
     //expect.assertions(2);
@@ -25,11 +22,38 @@ describe('Testes da pagina de receitas em progresso', () => {
     userEvent.type(emailInput, 'test@test.com');
     userEvent.type(passInput, '1234567');
     userEvent.click(loginBtn);
-    history.push('/foods/52771/in-progress');
-    const foodImg = screen.getByTestId("recipe-photo");
-    expect(foodImg).toHaveAttribute('src', 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg');
     history.push('/foods/178319/in-progress');
     const drinkImg = screen.getByTestId("recipe-photo");
     expect(drinkImg).toHaveAttribute('src', 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg');
+  });
+  it('Testa se, após clicar em compartilhar receita um link é copiado para a área de transferência', async () => {
+    const { history } = renderWithRouterAndContext(<App />);    
+    history.push('/foods/52771/in-progress');
+    const shareButton = screen.getByTestId('share-btn');
+    userEvent.click(shareButton);
+    mockClipboard.mockImplementation(() => null); 
+    expect(mockClipboard).toBeCalledTimes(1);
+    history.push('/foods/178319/in-progress');
+    const drinkImg = screen.getByTestId("recipe-photo");
+    expect(drinkImg).toHaveAttribute('src', 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg');
+  });
+  it('Testa se, após clicar no botão de favoritar o coração muda de white para black e a receita é salva em favoritos', async () => {
+    const { history } = renderWithRouterAndContext(<App />);    
+    history.push('/foods/52771/in-progress');
+    const favoriteButton = screen.getByTestId('favorite-btn');
+    expect(favoriteButton).toHaveAttribute('src', 'whiteHeartIcon.svg');
+    userEvent.click(favoriteButton);
+    expect(favoriteButton).toHaveAttribute('src', 'blackHeartIcon.svg');
+    userEvent.click(favoriteButton);
+    expect(favoriteButton).toHaveAttribute('src', 'whiteHeartIcon.svg');
+  });
+  it('Testa se, após marcar um ingrediente ele é salvo como concluído e ao desmarcar ele é removido da lista', async () => {
+    const { history } = renderWithRouterAndContext(<App />);    
+    history.push('/foods/52771/in-progress');    
+    const checkProgress = await screen.findByRole("checkbox", { name: /penne rigate \- 1 pound/i });
+    userEvent.click(checkProgress);
+    expect(checkProgress).toBeChecked();
+    userEvent.click(checkProgress);
+    expect(checkProgress).not.toBeChecked();
   });
 });
